434ea48c659705f860ef15ae238206aa7c7b3c11
hamed <abderahmanhamed@gmail.com>
2025-04-22 20:39:20 +0000

    Refactor Product interface to allow nullable stock, refund, deliverable, and warranty properties

--- src/data/models.ts
+++ src/data/models.ts
@@ -8,15 +8,26 @@ export interface Product {
     storeLogo: string,
     name: string;
     state: string;
+    stock: string | null;
+    refund: boolean | null;
     negotiable: boolean;
-    deliverable: boolean;
+    deliverable: boolean | null;
     orderable: boolean;
+    warranty: Warranty | null;
     details: string,
     price: number | null;
     discount: number;
     views: number;
+    satisfied: number;
+    unsatisfied: number;
     images: string[];
 
+}
+
+interface Warranty {
+    type: boolean;
+    duration: number;
+    unit: boolean;
 }
 
 export function setProduct(result: Models.Document): Product {
@@ -27,13 +38,18 @@ export function setProduct(result: Models.Document): Product {
         storeLogo: getStoreImageUrl(result.store.store_avatar),
         name: result.product_name,
         state: result.product_state,
+        stock: result.stock ?? null,
+        refund: result.refund ?? null,
         negotiable: result.product_negotiable,
         deliverable: result.product_deliverable ?? false,
         orderable: result.orderable ?? false,
+        warranty: result.warranty ?? null,
         details: result.product_details,
         price: result.product_price ?? null,
         discount: result.product_dicount_price ?? null,
         views: result.views ?? 0,
+        satisfied: result.satisfied ?? 0,
+        unsatisfied: result.unsatisfied ?? 0,
         images: result.product_imgs.map((img: any) => getProductImageUrl(img)),
     };
 }
