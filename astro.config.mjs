5f107dc40ae619e0e55c7699d0e30eec6b6a754e
hamed <abderahmanhamed@gmail.com>
2026-05-21 15:12:40 +0000

    Correct site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://market.ajbarly.app',
+  site: 'https://ajbarly.app',
 
   integrations: [
       sitemap()
